ffdeb4c11afb3434ce8cb1f62bb37a3d5caf80d4
DIvesh <diveshkswn@gmail.com>
2021-04-26 23:21:02 +0530

    cors update

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(express.json());
 // app.use((req, res, next) => {
 //   res.header('Access-Control-Allow-Origin', 'https://bootcamps-by-divesh.netlify.app'); // update to match the domain you will make the request from
 //   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+// res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
 //   next();
 // });
 
